66d1d4500325ced4d6a4b012f71b95a0a9687b44
Michael Hare <mphare@whitehare.com>
2014-02-10 23:02:38 -0600

    Added to the duck sim

--- Strategy/src/com/whitehare/hsdp/strategy/main/MiniDuckSim.java
+++ Strategy/src/com/whitehare/hsdp/strategy/main/MiniDuckSim.java
@@ -3,24 +3,32 @@ package com.whitehare.hsdp.strategy.main;
 import com.whitehare.hsdp.strategy.Duck;
 import com.whitehare.hsdp.strategy.FlyWithMotor;
 import com.whitehare.hsdp.strategy.MallardDuck;
+import com.whitehare.hsdp.strategy.Silent;
 import com.whitehare.hsdp.strategy.model.ModelDuck;
 
-public class MiniDuckSim {
+public class MiniDuckSim
+{
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Duck duck = new MallardDuck();
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    //
+    Duck duck = new MallardDuck();
+    duck.display();
+    duck.performQuack();
+    duck.performFly();
 
-		duck.performQuack();
-		duck.performFly();
+    //
+    Duck model = new ModelDuck();
+    model.display();
+    model.performFly();
+    model.setFlyBehavior(new FlyWithMotor());
+    model.performFly();
+    model.setQuackBehavior(new Silent());
+    model.performQuack();
 
-		Duck model = new ModelDuck();
-		model.performFly();
-		model.setFlyBehavior(new FlyWithMotor());
-		model.performFly();
-
-	}
+  }
 
 }
