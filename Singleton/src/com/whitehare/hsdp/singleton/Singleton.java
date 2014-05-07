3960007e819d18268ccf7e05ef49d9e9a653443f
Michael Hare <mphare@whitehare.com>
2014-05-06 21:30:58 -0500

    Added Log4j

--- Singleton/src/com/whitehare/hsdp/singleton/Singleton.java
+++ Singleton/src/com/whitehare/hsdp/singleton/Singleton.java
@@ -1,9 +1,12 @@
 package com.whitehare.hsdp.singleton;
 
+import org.apache.log4j.Logger;
+
 public class Singleton
 {
   private static Singleton uniqueInstance;
   private int              uniqueInteger;
+  static Logger            log4j = Logger.getLogger(Singleton.class);
 
   private Singleton()
   {
@@ -13,8 +16,10 @@ public class Singleton
   {
     if (uniqueInstance == null)
     {
+      log4j.debug("Creating Singleton");
       uniqueInstance = new Singleton();
     }
+    log4j.debug("Returning Instance");
     return uniqueInstance;
   }
 
