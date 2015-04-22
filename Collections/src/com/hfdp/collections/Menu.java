ab88d0b666a9e9d75e4274128d9211b16fccf1bc
Michael Hare <mphare@whitehare.com>
2015-04-21 23:01:23 -0500

    use java iterators

---
+++ Collections/src/com/hfdp/collections/Menu.java
@@ -0,0 +1,6 @@
+package com.hfdp.collections;
+
+public interface Menu
+{
+  public Iterator createIterator();
+}
--- Collections/src/com/hfdp/collections/test/WhenTestingWaitress.java
+++
@@ -1,6 +0,0 @@
-package com.hfdp.collections.test;
-
-public class WhenTestingWaitress
-{
-
-}
