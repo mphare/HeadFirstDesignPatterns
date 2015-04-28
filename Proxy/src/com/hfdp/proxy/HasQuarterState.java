8641ade32902cfa0c05ee1d08d660624b6b7dc13
Michael Hare <mphare@whitehare.com>
2015-04-27 20:30:33 -0500

    Add Proxy Pattern, derived from State Pattern

---
+++ Proxy/src/com/hfdp/proxy/HasQuarterState.java
@@ -0,0 +1,40 @@
+package com.hfdp.proxy;
+
+public class HasQuarterState implements State
+{
+
+  GumballMachine gumballMachine;
+
+  public HasQuarterState(GumballMachine gumballMachine)
+  {
+    this.gumballMachine = gumballMachine;
+  }
+
+  @Override
+  public void ejectQuarter()
+  {
+    System.out.println("Quarter returned");
+    gumballMachine.setState(gumballMachine.getNoQuarterState());
+
+  }
+
+  @Override
+  public void turnCrank()
+  {
+
+  }
+
+  @Override
+  public void dispence()
+  {
+
+  }
+
+  @Override
+  public void insertQuarter()
+  {
+    System.out.println("You cannot insert another quarter");
+
+  }
+
+}
---
+++ Proxy/src/com/hfdp/proxy/State.java
@@ -0,0 +1,14 @@
+package com.hfdp.proxy;
+
+public interface State
+{
+
+  public void ejectQuarter();
+
+  public void turnCrank();
+
+  public void dispence();
+
+  public void insertQuarter();
+
+}
