6c8b0bfe3c16d730acbeee25bb709f3803574eda
Michael Hare <mhare@zixcorp.com>
2015-04-29 16:32:37 -0500

    Added more Unit Tests

--- State/src/com/whitehare/hsdp/state/test/WhenTestingMachine.java
+++ State/src/com/whitehare/hsdp/state/test/WhenTestingMachine.java
@@ -3,7 +3,6 @@ package com.whitehare.hsdp.state.test;
 import org.junit.Test;
 
 import com.whitehare.hsdp.state.GumballMachine;
-import com.whitehare.hsdp.state.State;
 
 public class WhenTestingMachine
 {
@@ -28,15 +27,50 @@ public class WhenTestingMachine
   }
 
   @Test
+  public void shouldExerciseSoldOutMachine()
+  {
+    GumballMachine machine = new GumballMachine(5);
+    System.out.println(machine);
+    machine.insertQuarter();
+    machine.turnCrank();
+    machine.insertQuarter();
+    machine.turnCrank();
+    machine.insertQuarter();
+    machine.turnCrank();
+    machine.insertQuarter();
+    machine.turnCrank();
+    machine.insertQuarter();
+    machine.turnCrank();
+    machine.insertQuarter();
+    machine.turnCrank();
+  }
+
+  @Test
   public void shouldRejectCrankWithoutQuarter()
   {
     GumballMachine machine = new GumballMachine(5);
     System.out.println(machine);
     machine.turnCrank();
-    System.out.println(machine);
-    State state = machine.getState();
-    System.out.println("State: " + state);
 
   }
 
+  @Test
+  public void shouldRejectInsertTwoQuarters()
+  {
+    GumballMachine machine = new GumballMachine(5);
+    System.out.println(machine);
+    machine.insertQuarter();
+    machine.insertQuarter();
+
+  }
+
+  @Test
+  public void shouldEjectQuarter()
+  {
+    GumballMachine machine = new GumballMachine(5);
+    System.out.println(machine);
+    machine.insertQuarter();
+    machine.ejectQuarter();
+
+  }
 }
